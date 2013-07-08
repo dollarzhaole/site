035645b3bc8bef7d5c90057d3e3c5fbfe1b1eca2
zhaole <dollar_wish@outlook.com>
2013-07-08 14:39:13 +0800

    all

--- 2/index.py
+++ 2/index.py
@@ -1,11 +1,14 @@
 #-*- coding:utf-8 -*-
+import os
+import sys
 
-def app(environ, start_response):
-    status = '200 OK'
-    headers = [('Content-type', 'text/html')]
-    start_response(status, headers)
-    body=["Welcome to Baidu Cloud!\n"]
-    return body
+os.environ['DJANGO_SETTINGS_MODULE'] = 'djangodemo.settings'
 
+path = os.path.dirname(os.path.abspath(__file__)) + '/djangodemo'
+if path not in sys.path:
+    sys.path.insert(1, path)
+
+from django.core.handlers.wsgi import WSGIHandler
 from bae.core.wsgi import WSGIApplication
-application = WSGIApplication(app)
+
+application = WSGIApplication(WSGIHandler())
